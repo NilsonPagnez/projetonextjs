a0e4fc0248bca26cec39b45eb9acd47acb1d3b99
Nilson Pagnez <nilson.pagneto@gmail.com>
2023-09-06 16:17:53 -0300

    adicionando fonte e headers às páginas

--- pages/faq.js
+++ pages/faq.js
@@ -1,22 +1,45 @@
-import { useEffect } from "react"
+import Head from "next/head"
+import { useEffect, useState } from "react"
 import Link from "../src/components/Link"
 
-export default function faq(){
-    useEffect(()=>{
-        const FAQ_API_URL ='https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
-        fetch(FAQ_API_URL)
-        .then((response)=> response.json())
-        .then((response) => console.log(response))
-    }, [])
-    return(
-        <>
+
+export async function getStaticProps() {
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+    const getFaq = await fetch(FAQ_API_URL)
+        .then((response) => { return response.json() })
+        .then((response) => { return response })
+
+    return {
+        props: {
+            getFaq,
+        }
+    }
+}
+
+export default function faq({ getFaq }) {
+    console.log(getFaq)
+    return (
+        <div>
+            <Head>
+                <title>FAQ - Alura Cases Campanha</title>
+            </Head>
             <h1>
                 faq
             </h1>
             <Link href='/'>
                 Homepage
             </Link>
-        </>
+            <ul>
+                {getFaq.map(({ answer, question }) => (
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
+        </div>
 
     )
 }
--- src/components/Patterns/Footer/index.js
+++ src/components/Patterns/Footer/index.js
@@ -1,5 +1,5 @@
-import { Box, Text } from '../../../theme/components';
-import { theme } from '../../../theme/theme';
+import { Box, Text } from "../../../theme/components";
+import { theme } from "../../../theme/theme";
 
 export default function Footer() {
     return (
